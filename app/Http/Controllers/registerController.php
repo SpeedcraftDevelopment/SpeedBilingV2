75ec47d8eefeca47580c36870be5b981d0acc721
ZiomekMinecraft <paszkietpawel@wp.pl>
2024-05-01 00:55:29 +0200

    Register user system and repair login system

---
+++ app/Http/Controllers/registerController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class registerController extends Controller
+{
+    function get(){
+        if(Auth::check()){
+            return redirect()->intended('/');
+        }
+        return view("user.register");
+    }
+
+    function post(Request $request){
+        
+        $validated = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'rpassword' => 'required',
+        ]);
+
+        if(!$validated){
+            return back()->withErrors([
+                'data' => "Sended data is not correct",
+            ]);
+        }
+
+        if(User::where('email', $request->input("email"))->first()){
+            return back()->withErrors([
+                'email' => "Account with that e-mail already exists!",
+            ]);
+        }
+        if(User::where('name', $request->input("name"))->first()){
+            return back()->withErrors([
+                'name' => "Account with that name already exists!",
+            ]);
+        }
+        if($validated["password"]!==$validated["rpassword"]){
+            return back()->withErrors([
+                'password' => "Passwords are not the same!",
+            ]);
+        }
+
+        $user = new User();
+
+        $user->name = $validated["name"];
+        $user->email = $validated["email"];
+        $user->password = $validated["password"];
+
+        $user->save();
+
+        Auth::login($user);
+
+        return redirect("/");
+    }
+}
---
+++ resources/views/user/register.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.main')
+
+@section('title')
+    Register
+@endsection
+
+@section('content')
+<form action="/register" method="post">
+    @csrf
+    <label for="name">Name</label><br>
+    <input type="text" name="name" id="name" required><br><br>
+    <label for="email">E-Mail</label><br>
+    <input type="email" name="email" id="email" required><br><br>
+    <label for="password">Password</label><br>
+    <input type="password" name="password" id="password" required><br><br>
+    <label for="rpassword">Repeat password</label><br>
+    <input type="password" name="rpassword" id="rpassword" required><br><br>
+    <button>Login</button>
+</form>
+@if ($errors->any())
+    <div>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+@endsection
